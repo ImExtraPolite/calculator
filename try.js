b3561766f4d469deae9099f59239b7ed9b2b5841
ImExtraPolite <jaleel1666@gmail.com>
2023-04-27 18:30:08 -0400

    start javascript over

--- try.js
+++ try.js
@@ -1,19 +1,1 @@
-function add(num1, num2) {
-  return num1 + " + " + num2 + " = " + (num1 + num2);
-}
 
-function subtract(num1, num2) {
-  return num1 + " - " + num2 + " = " + (num1 - num2);
-}
-
-function multiply(num1, num2) {
-  return num1 + " * " + num2 + " = " + (num1 * num2);
-}
-
-function divide(num1, num2) {
-  return num1 + " / " + num2 + " = " + (num1 / num2);
-}
-
-function operate(num1, operation, num2) {
-  
-}
