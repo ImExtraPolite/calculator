533f55d4d51f7dceb3defff3cd1b7f40a957d86e
ImExtraPolite <jaleel1666@gmail.com>
2023-05-29 16:37:18 -0400

    changes

--- try.js
+++ try.js
@@ -14,6 +14,21 @@ function divide(num1, num2) {
   return num1 + " / " + num2 + " = " + (num1 / num2);
 }
 
-function operate(num1, operation, num2) {
-  
+function operate(num1, num2, operator) {
+  if (operator == "+") {
+    add(num1, num2);
+  } else if (operator == "-") {
+    subtract (num1, num2) 
+  } else if (operator == "*") {
+    multiply(num1, num2);
+  } else if (operator == "/") {
+    divide(num1, num2);
+  } else {
+    return "wrong operator!";
+  }
 }
+
+let num1;
+let num2;
+let operator;
+
